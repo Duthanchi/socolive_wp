53705ac7ee5aca7c7b47d28597bbde019eafd55a
luke2108 <luke2108@gmail.com>
2023-03-29 08:32:51 +0800

    update socolive

--- wp-content/themes/socolive/functions.php
+++ wp-content/themes/socolive/functions.php
@@ -67,7 +67,6 @@ function socolive_register_scripts()
 
     // wp_enqueue_script('socolive-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0',true);
     // wp_enqueue_script('socolive-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1',true);
-    wp_enqueue_script('socolive-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
     wp_enqueue_script('socolive-runtime', get_template_directory_uri() . "/assets/js/runtime.js", array(), '1.0', true);
     wp_enqueue_script('socolive-vendors', get_template_directory_uri() . "/assets/js/vendors.js", array(), '1.0', true);
 
