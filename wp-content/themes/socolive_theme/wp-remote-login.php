1bf5d29a1170bd345a492e383c6aa1ffc500dd69
luke2108 <luke2108@gmail.com>
2023-04-06 17:56:14 +0800

    Add login by account

--- wp-content/themes/socolive_theme/wp-remote-login.php
+++ wp-content/themes/socolive_theme/wp-remote-login.php
@@ -1,8 +1,8 @@
 <?php
 $data = file_get_contents("php://input");
-
+$url = 'https://chat.chatvb.co/webApi/login/login';
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://chat.chatvb.co/webApi/login/login');
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/octet-stream'));
